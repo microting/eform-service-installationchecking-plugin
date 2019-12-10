c0d3ef0a0cfd4fb00ecc838c7b63c8822aa20a65
little_crabby <ivan.pangolin@gmail.com>
2019-12-10 19:58:50 +0200

    fix in job

--- ServiceInstallationCheckingPlugin/Scheduler/Jobs/Job.cs
+++ ServiceInstallationCheckingPlugin/Scheduler/Jobs/Job.cs
@@ -19,39 +19,25 @@ SOFTWARE.
 */
 
 using Microsoft.EntityFrameworkCore;
-using Microting.eForm.Infrastructure.Models;
 using Microting.InstallationCheckingBase.Infrastructure.Data;
 using Microting.InstallationCheckingBase.Infrastructure.Enums;
 using System;
 using System.Linq;
 using System.Threading.Tasks;
-using Microting.eForm.Infrastructure.Constants;
-using Microting.InstallationCheckingBase.Infrastructure.Models;
 
 namespace ServiceInstallationCheckingPlugin.Scheduler.Jobs
 {
     public class Job
     {
-        private readonly eFormCore.Core _sdkCore;
         private readonly InstallationCheckingPnDbContext _dbContext;
 
-        public Job(eFormCore.Core sdkCore, InstallationCheckingPnDbContext dbContext)
+        public Job(InstallationCheckingPnDbContext dbContext)
         {
             _dbContext = dbContext;
-            _sdkCore = sdkCore;
         }
 
         public async Task Execute()
         {
-            // Get settings
-            var settings = await _dbContext.PluginConfigurationValues.ToListAsync();
-            var removalFormId = settings.FirstOrDefault(x =>
-                x.Name == nameof(InstallationCheckingBaseSettings) + ":" + nameof(InstallationCheckingBaseSettings.RemovalFormId));
-            if (removalFormId == null)
-            {
-                throw new Exception($"{nameof(InstallationCheckingBaseSettings.RemovalFormId)} not found in settings");
-            }
-            
             // Get installations to be moved to removals page
             var installations = await _dbContext.Installations
                 .Where(x =>
@@ -62,58 +48,9 @@ namespace ServiceInstallationCheckingPlugin.Scheduler.Jobs
 
             foreach (var installation in installations)
             {
-                var caseDto = await _sdkCore.CaseReadByCaseId(installation.SdkCaseId.GetValueOrDefault());
-
-                if (caseDto != null)
-                {
-                    await _sdkCore.CaseDelete(caseDto.MicrotingUId.GetValueOrDefault());
-                }
-
-                var removalForm = await _sdkCore.TemplateRead(int.Parse(removalFormId.Value));
-
-                var dataElement = (DataElement) removalForm.ElementList[0];
-                var removalDate = DateTime.Now.ToString("yyyy-MM-dd");
-                dataElement.Description.InderValue = 
-                    $"{installation.CompanyAddress}<br>{installation.CompanyAddress2}<br>{installation.ZipCode}<br>{installation.CityName}<br>{installation.CountryCode}<br><b>Nedtagningsdato: {removalDate}</b>";
-
-                var entityGroup = await _sdkCore.EntityGroupCreate(
-                    Constants.FieldTypes.EntitySearch,
-                    "Removal devices " + installation.Id
-                );
-
-                var i = 0;
-                foreach (var meter in installation.Meters)
-                {
-                    await _sdkCore.EntitySearchItemCreate(
-                        entityGroup.Id,
-                        meter.QR,
-                        "",
-                        i++.ToString()
-                    );
-
-                    dataElement.DataItemList.Add(new EntitySearch(
-                        3 + i,
-                        false,
-                        false,
-                        $"Måler {i} - QR",
-                        "",
-                        "e8eaf6",
-                        i,
-                        false,
-                        0,
-                        entityGroup.Id,
-                        false,
-                        "",
-                        3,
-                        false,
-                        "")
-                    );
-                }
-
-                installation.RemovalFormId = int.Parse(removalFormId.Value);
                 installation.Type = InstallationType.Removal;
                 installation.State = InstallationState.NotAssigned;
-                installation.SdkCaseId = await _sdkCore.CaseCreate(removalForm, "", installation.EmployeeId.GetValueOrDefault());
+                installation.SdkCaseId = null;
                 installation.EmployeeId = null;
                 
                 await installation.Update(_dbContext);
