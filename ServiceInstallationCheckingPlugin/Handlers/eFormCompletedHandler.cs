b0549aa50c7434e4b071364dabd71573e28afe69
Leonid <leonid.atf@gmail.com>
2019-12-11 14:44:08 +0200

    Small refactoring

--- ServiceInstallationCheckingPlugin/Handlers/eFormCompletedHandler.cs
+++ ServiceInstallationCheckingPlugin/Handlers/eFormCompletedHandler.cs
@@ -25,11 +25,11 @@ namespace ServiceInstallationCheckingPlugin.Handlers
             _dbContext = dbContext;
             _sdkCore = sdkCore;
         }
-        
+
         public async Task Handle(eFormCompleted message)
         {
             var installation = await _dbContext.Installations
-                .FirstOrDefaultAsync(x => 
+                .FirstOrDefaultAsync(x =>
                     x.State == InstallationState.Assigned &&
                     x.SdkCaseId == message.microtingUId
                 );
@@ -41,52 +41,77 @@ namespace ServiceInstallationCheckingPlugin.Handlers
                 var replyElement = await _sdkCore.CaseRead(message.microtingUId, message.checkUId);
                 var checkListValue = (CheckListValue) replyElement.ElementList[0];
                 var fields = checkListValue.DataItemList.Select(di => di as Field).ToList();
-
-                installation.CadastralNumber = fields[0].FieldValues[0].Value;
-                installation.PropertyNumber = fields[1].FieldValues[0].Value;
-                installation.ApartmentNumber = fields[2].FieldValues[0].Value;
-                installation.CadastralType = fields[3].FieldValues[0].ValueReadable;
-                installation.YearBuilt = int.Parse(fields[4].FieldValues[0].Value);
-                installation.LivingFloorsNumber = int.Parse(fields[5].FieldValues[0].Value);
-
-                foreach (var field in fields.Skip(8))
+                if (fields.Any())
                 {
-                    var rgx = new Regex(@"Måler (?<Num>\d*) - (?<Name>.*)");
-                    var match = rgx.Match(field.Label);
-
-                    if (!match.Success || field.FieldValues[0].Value.IsNullOrEmpty()) continue;
-
-                    var num = int.Parse(match.Groups["Num"].Value);
-                    var name = match.Groups["Name"].Value;
-                    
-                    var meter = installation.Meters.FirstOrDefault(m => m.Num == num)
-                                ?? new Meter() { Num = num };
-
-                    switch (name)
+                    if (!string.IsNullOrEmpty(fields[0]?.FieldValues[0]?.Value))
                     {
-                        case "QR":
-                            meter.QR = field.FieldValues[0].Value;
-                            break;
-                        case "Rumtype":
-                            meter.RoomType = field.FieldValues[0].ValueReadable;
-                            break;
-                        case "Etage":
-                            meter.Floor = int.Parse(field.FieldValues[0].Value);
-                            break;
-                        case "Rumnavn":
-                            meter.RoomName = field.FieldValues[0].Value;
-                            break;
+                        installation.CadastralNumber = fields[0]?.FieldValues[0]?.Value;
                     }
 
-                    if (installation.Meters.All(m => m.Num != num))
+                    if (!string.IsNullOrEmpty(fields[1]?.FieldValues[0]?.Value))
                     {
-                        installation.Meters.Add(meter);
+                        installation.PropertyNumber = fields[1].FieldValues[0].Value;
+                    }
+
+                    if (!string.IsNullOrEmpty(fields[2]?.FieldValues[0]?.Value))
+                    {
+                        installation.ApartmentNumber = fields[2].FieldValues[0].Value;
+                    }
+
+                    if (!string.IsNullOrEmpty(fields[3]?.FieldValues[0]?.ValueReadable))
+                    {
+                        installation.CadastralType = fields[3].FieldValues[0].ValueReadable;
+                    }
+
+                    if (!string.IsNullOrEmpty(fields[4]?.FieldValues[0]?.Value))
+                    {
+                        installation.YearBuilt = int.Parse(fields[4].FieldValues[0].Value);
+                    }
+
+                    if (!string.IsNullOrEmpty(fields[5]?.FieldValues[0]?.Value))
+                    {
+                        installation.LivingFloorsNumber = int.Parse(fields[5].FieldValues[0].Value);
+                    }
+
+                    foreach (var field in fields.Skip(8))
+                    {
+                        var rgx = new Regex(@"Måler (?<Num>\d*) - (?<Name>.*)");
+                        var match = rgx.Match(field.Label);
+
+                        if (!match.Success || field.FieldValues[0].Value.IsNullOrEmpty()) continue;
+
+                        var num = int.Parse(match.Groups["Num"]?.Value);
+                        var name = match.Groups["Name"].Value;
+
+                        var meter = installation.Meters.FirstOrDefault(m => m.Num == num)
+                                    ?? new Meter() {Num = num};
+
+                        switch (name)
+                        {
+                            case "QR":
+                                meter.QR = field.FieldValues[0].Value;
+                                break;
+                            case "Rumtype":
+                                meter.RoomType = field.FieldValues[0].ValueReadable;
+                                break;
+                            case "Etage":
+                                meter.Floor = int.Parse(field.FieldValues[0].Value);
+                                break;
+                            case "Rumnavn":
+                                meter.RoomName = field.FieldValues[0].Value;
+                                break;
+                        }
+
+                        if (installation.Meters.All(m => m.Num != num))
+                        {
+                            installation.Meters.Add(meter);
+                        }
                     }
                 }
 
                 installation.DateInstall = DateTime.UtcNow;
                 installation.DateRemove = DateTime.UtcNow.AddDays(DaysBeforeRemove);
-            } 
+            }
             else
             {
                 installation.DateActRemove = DateTime.UtcNow;
