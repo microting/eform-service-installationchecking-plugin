27fc243708d7bbd1cd92ed4f3fbc4d9b2cc27b07
René Schultz Madsen <rm@microting.dk>
2021-02-23 13:10:11 +0000

    Adding changes to make it use Danish language.

--- ServiceInstallationCheckingPlugin/Handlers/eFormCompletedHandler.cs
+++ ServiceInstallationCheckingPlugin/Handlers/eFormCompletedHandler.cs
@@ -3,13 +3,17 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using Microting.eForm.Infrastructure.Models;
+using Microting.eForm.Infrastructure;
+using Microting.eForm.Infrastructure.Data.Entities;
 using Microting.InstallationCheckingBase.Infrastructure.Data;
 using Microting.InstallationCheckingBase.Infrastructure.Data.Entities;
 using Microting.InstallationCheckingBase.Infrastructure.Enums;
 using Rebus.Handlers;
 using ServiceInstallationCheckingPlugin.Messages;
 using OpenStack.NetCoreSwiftClient.Extensions;
+using CheckListValue = Microting.eForm.Infrastructure.Models.CheckListValue;
+using Field = Microting.eForm.Infrastructure.Models.Field;
+using FieldValue = Microting.eForm.Infrastructure.Models.FieldValue;
 
 namespace ServiceInstallationCheckingPlugin.Handlers
 {
@@ -36,12 +40,14 @@ namespace ServiceInstallationCheckingPlugin.Handlers
                 );
 
             if (installation == null) return;
+            await using MicrotingDbContext microtingDbContext = _sdkCore.DbContextHelper.GetDbContext();
+            Language language = await microtingDbContext.Languages.SingleAsync(x => x.LanguageCode == "da");
 
             Console.WriteLine("[INF] EFormCompletedHandler.Handle: installation != null");
             if (installation.Type == InstallationType.Installation)
             {
                 Console.WriteLine("[INF] EFormCompletedHandler.Handle: installation.Type == InstallationType.Installation");
-                var replyElement = await _sdkCore.CaseRead(message.microtingUId, message.checkUId);
+                var replyElement = await _sdkCore.CaseRead(message.microtingUId, message.checkUId, language);
                 var checkListValue = (CheckListValue) replyElement.ElementList[0];
                 var fields = checkListValue.DataItemList.Select(di => di as Field).ToList();
                 if (fields.Any())
@@ -95,7 +101,7 @@ namespace ServiceInstallationCheckingPlugin.Handlers
                                 {
                                     Console.WriteLine($"[ERR] EFormCompletedHandler.Handle: InstallationImageName += gave error {ex.Message}");
                                 }
-                                
+
                             }
                         }
                     }
@@ -103,7 +109,7 @@ namespace ServiceInstallationCheckingPlugin.Handlers
                     Console.WriteLine("[INF] EFormCompletedHandler.Handle: start looping fields");
                     foreach (var field in fields.Skip(8))
                     {
-                        
+
                         Console.WriteLine($"[INF] EFormCompletedHandler.Handle: parsing field {field.Label}");
                         var rgx = new Regex(@"Måler (?<Num>\d*) - (?<Name>.*)");
                         var match = rgx.Match(field.Label);
@@ -142,7 +148,7 @@ namespace ServiceInstallationCheckingPlugin.Handlers
 
                 installation.DateInstall = DateTime.UtcNow;
                 installation.DateRemove = DateTime.UtcNow.AddDays(DaysBeforeRemove);
-                
+
                 await _sdkCore.CaseDelete(message.microtingUId);
 
                 installation.Type = InstallationType.Removal;
@@ -152,7 +158,7 @@ namespace ServiceInstallationCheckingPlugin.Handlers
             else
             {
                 installation.DateActRemove = DateTime.UtcNow;
-                
+
                 await _sdkCore.CaseDelete(message.microtingUId);
 
                 installation.State = InstallationState.Completed;
